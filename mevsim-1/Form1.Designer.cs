c4640175539f1151d525e2d60c9638aac1cfb901
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-10-16 15:42:42 +0300

    Add files via upload

---
+++ mevsim-1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace mevsim_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbMevsimler = new System.Windows.Forms.ComboBox();
+            this.lstAylar = new System.Windows.Forms.ListBox();
+            this.cmbAylar = new System.Windows.Forms.ComboBox();
+            this.lblMevsim = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbMevsimler
+            // 
+            this.cmbMevsimler.FormattingEnabled = true;
+            this.cmbMevsimler.Items.AddRange(new object[] {
+            "İlkbahar",
+            "Yaz",
+            "Sonbahar",
+            "Kış"});
+            this.cmbMevsimler.Location = new System.Drawing.Point(80, 57);
+            this.cmbMevsimler.Name = "cmbMevsimler";
+            this.cmbMevsimler.Size = new System.Drawing.Size(209, 32);
+            this.cmbMevsimler.TabIndex = 0;
+            this.cmbMevsimler.SelectedIndexChanged += new System.EventHandler(this.cmbMevsimler_SelectedIndexChanged);
+            // 
+            // lstAylar
+            // 
+            this.lstAylar.FormattingEnabled = true;
+            this.lstAylar.ItemHeight = 24;
+            this.lstAylar.Location = new System.Drawing.Point(295, 57);
+            this.lstAylar.Name = "lstAylar";
+            this.lstAylar.Size = new System.Drawing.Size(194, 196);
+            this.lstAylar.TabIndex = 1;
+            // 
+            // cmbAylar
+            // 
+            this.cmbAylar.FormattingEnabled = true;
+            this.cmbAylar.Items.AddRange(new object[] {
+            "Ocak",
+            "Şubat",
+            "Mart",
+            "Nisan",
+            "Mayıs",
+            "Haziran",
+            "Temmuz",
+            "Ağustos",
+            "Eylül",
+            "Ekim",
+            "Kasım",
+            "Aralık"});
+            this.cmbAylar.Location = new System.Drawing.Point(568, 57);
+            this.cmbAylar.Name = "cmbAylar";
+            this.cmbAylar.Size = new System.Drawing.Size(209, 32);
+            this.cmbAylar.TabIndex = 2;
+            this.cmbAylar.SelectedIndexChanged += new System.EventHandler(this.cmbAylar_SelectedIndexChanged);
+            // 
+            // lblMevsim
+            // 
+            this.lblMevsim.AutoSize = true;
+            this.lblMevsim.Location = new System.Drawing.Point(808, 60);
+            this.lblMevsim.Name = "lblMevsim";
+            this.lblMevsim.Size = new System.Drawing.Size(60, 24);
+            this.lblMevsim.TabIndex = 3;
+            this.lblMevsim.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 475);
+            this.Controls.Add(this.lblMevsim);
+            this.Controls.Add(this.cmbAylar);
+            this.Controls.Add(this.lstAylar);
+            this.Controls.Add(this.cmbMevsimler);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbMevsimler;
+        private System.Windows.Forms.ListBox lstAylar;
+        private System.Windows.Forms.ComboBox cmbAylar;
+        private System.Windows.Forms.Label lblMevsim;
+    }
+}
+
---
+++ mevsim-1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mevsim_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbMevsimler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int secim=cmbMevsimler.SelectedIndex;
+
+            if (secim==0)
+            {
+                lstAylar.Items.Clear();
+                lstAylar.Items.Add("Mart");
+                lstAylar.Items.Add("Nisan");
+                lstAylar.Items.Add("Mayıs");
+            }
+            else if(secim==1)
+            {
+                lstAylar.Items.Clear();
+                lstAylar.Items.Add("Haziran");
+                lstAylar.Items.Add("Temmuz");
+                lstAylar.Items.Add("Ağustos");
+            }
+            else if (secim == 2)
+            {
+                lstAylar.Items.Clear();
+                lstAylar.Items.Add("Eylül");
+                lstAylar.Items.Add("Ekim");
+                lstAylar.Items.Add("Kasım");
+            }
+            else
+            {
+                lstAylar.Items.Clear ();
+                lstAylar.Items.Add("Aralık");
+                lstAylar.Items.Add("Ocak");
+                lstAylar.Items.Add("Şubat");
+            }
+        }
+
+        private void cmbAylar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int secim=cmbAylar.SelectedIndex;
+
+            if(secim==2 || secim==3 || secim == 4) //Mart Nisan Mayıs
+            {
+                lblMevsim.Text = "İlkbahar";
+            }
+            else if(secim==5 || secim==6|| secim == 7) //Haziran Temmuz Ağustos
+            {
+                lblMevsim.Text = "Yaz";
+            }
+            else if(secim==8 || secim==9|| secim == 10) //Eylül Ekim Kasım
+            {
+                lblMevsim.Text = "Sonbahar";
+            }
+            else  //Diğer geri kalan durumlar
+            {
+                lblMevsim.Text = "Kış";
+            }
+
+
+        }
+    }
+}
